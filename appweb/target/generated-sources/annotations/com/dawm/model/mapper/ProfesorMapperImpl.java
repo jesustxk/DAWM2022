d1fccb06ac5766e8c558fc8d79f854c607eaf065
Jesús Checa <jesuscheca11@gmail.com>
2023-01-15 19:14:55 +0100

    todo roto ole ole

--- appweb/target/generated-sources/annotations/com/dawm/model/mapper/ProfesorMapperImpl.java
+++ appweb/target/generated-sources/annotations/com/dawm/model/mapper/ProfesorMapperImpl.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2023-01-15T18:37:56+0100",
+    date = "2023-01-15T19:11:48+0100",
     comments = "version: 1.4.2.Final, compiler: Eclipse JDT (IDE) 1.4.200.v20221012-0724, environment: Java 17.0.5 (Eclipse Adoptium)"
 )
 @Component
