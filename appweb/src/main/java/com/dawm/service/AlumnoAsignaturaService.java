53784a18951a5332781b2f3394003285569371e7
Jesús Checa <jesuscheca11@gmail.com>
2023-01-18 19:22:35 +0100

    feat: Reestructurando la base de datos

--- appweb/src/main/java/com/dawm/service/AlumnoAsignaturaService.java
+++
@@ -1,9 +0,0 @@
-package com.dawm.service;
-
-import com.dawm.model.dto.AlumnoAsignaturaDTO;
-
-public interface AlumnoAsignaturaService {
-    
-    public Long insertAlumnoAsignatura(AlumnoAsignaturaDTO alumnoAsignaturaDTO);
-
-}
