f210122c5ea4ad6d65721ebaaf8465b15d2d890f
Jesús Checa <jesuscheca11@gmail.com>
2023-01-30 17:01:29 +0100

    fix: getUsuario

--- appweb/src/main/java/com/dawm/repository/UsuarioRepository.java
+++ appweb/src/main/java/com/dawm/repository/UsuarioRepository.java
@@ -2,6 +2,7 @@ package com.dawm.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.dawm.model.entity.Usuario;
@@ -10,6 +11,6 @@ import com.dawm.model.entity.Usuario;
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
 
     @Query(value = "SELECT u.* FROM USERS u WHERE u.USERNAME = :username", nativeQuery = true)
-    Usuario findByUsername(String username);
+    Usuario findByUsername(@Param("username") String username);
 
 }
